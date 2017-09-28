3affeb88b138526d812071ec52f75b5ac766ad4f
valdeweb <tallervaldeweb@gmail.com>
2017-09-28 14:55:57 +0200

    añado contenido

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('no').onclick=function(){
+    document.getElementById('bienvenida').style.display='none';
+     document.getElementById('noQuieroJugar').style.display='block';
+}
+document.getElementById('si').onclick=function(){
+    document.getElementById('bienvenida').style.display='none';
+     document.getElementById('elegirParesNones').style.display='block';
+}
