87bbb02ad4842441464fbdff37010d26b0d28541
valdeweb <tallervaldeweb@gmail.com>
2017-09-29 09:12:38 +0200

    hago cambios

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('no').onclick=function(){
-    document.getElementById('bienvenida').style.display='none';
-     document.getElementById('noQuieroJugar').style.display='block';
-}
-document.getElementById('si').onclick=function(){
-    document.getElementById('bienvenida').style.display='none';
-     document.getElementById('elegirParesNones').style.display='block';
-}
